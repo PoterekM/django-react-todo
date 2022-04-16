78c5d5318dd3c478a3a7009d72fb9c2d5c9baf15
Michelle Poterek <poterekm@users.noreply.github.com>
2022-04-16 12:35:24 -0700

    import custom modal and add click events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,115 +1,85 @@
 import React, { Component } from 'react';
+import {
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Form,
+	FormGroup,
+	Input,
+	Label,
+} from 'reactstrap';
 
-const todoItems = [
-	{
-		id: 1,
-		title: 'Go to Market',
-		description: 'Buy ingredients to prepare dinner',
-		completed: true,
-	},
-	{
-		id: 2,
-		title: 'Study',
-		description: 'Read Algebra and History textbook for the upcoming test',
-		completed: false,
-	},
-	{
-		id: 3,
-		title: "Sammy's books",
-		description: "Go to library to return Sammy's books",
-		completed: true,
-	},
-	{
-		id: 4,
-		title: 'Article',
-		description: 'Write article on how to use Django with React',
-		completed: false,
-	},
-];
-
-class App extends Component {
+export default class CustomModal extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			viewCompleted: false,
-			todoList: todoItems,
+			activeItem: this.props.activeItem,
 		};
 	}
 
-	displayCompleted = (status) => {
-		if (status) {
-			return this.setState({ viewCompleted: true });
+	handleChange = (e) => {
+		let { name, value } = e.target;
+
+		if (e.target.type === 'checkbox') {
+			value = e.target.checked;
 		}
 
-		return this.setState({ viewCompleted: false });
-	};
+		const activeItem = { ...this.state.activeItem, [name]: value };
 
-	renderTabList = () => {
-		return (
-			<div className='nav nav-tabs'>
-				<span
-					className={this.state.viewCompleted ? 'nav-link active' : 'nav-link'}
-					onClick={() => this.displayCompleted(true)}
-				>
-					Complete
-				</span>
-				<span
-					className={this.state.viewCompleted ? 'nav-link' : 'nav-link active'}
-					onClick={() => this.displayCompleted(false)}
-				>
-					Incomplete
-				</span>
-			</div>
-		);
-	};
-
-	renderItems = () => {
-		const { viewCompleted } = this.state;
-		const newItems = this.state.todoList.filter(
-			(item) => item.completed == viewCompleted
-		);
-
-		return newItems.map((item) => (
-			<li
-				key={item.id}
-				className='list-group-item d-flex justify-content-between align-items-center'
-			>
-				<span
-					className={`todo-title mr-2 ${
-						this.state.viewCompleted ? 'completed-todo' : ''
-					}`}
-					title={item.description}
-				>
-					{item.title}
-				</span>
-				<span>
-					<button className='btn btn-secondary mr-2'>Edit</button>
-					<button className='btn btn-danger'>Delete</button>
-				</span>
-			</li>
-		));
+		this.setState({ activeItem });
 	};
 
 	render() {
+		const { toggle, onSave } = this.props;
+
 		return (
-			<main className='container'>
-				<h1 className='text-white text-uppercase text-center my-4'>Todo app</h1>
-				<div className='row'>
-					<div className='col-md-6 col-sm-10 mx-auto p-0'>
-						<div className='card p-3'>
-							<div className='mb-4'>
-								<button className='btn btn-primary'>Add task</button>
-							</div>
-							{this.renderTabList()}
-							<ul className='list-group list-group-flush border-top-0'>
-								{this.renderItems()}
-							</ul>
-						</div>
-					</div>
-				</div>
-			</main>
+			<Modal isOpen={true} toggle={toggle}>
+				<ModalHeader toggle={toggle}>Todo Item</ModalHeader>
+				<ModalBody>
+					<Form>
+						<FormGroup>
+							<Label for='todo-title'>Title</Label>
+							<Input
+								type='text'
+								id='todo-title'
+								name='title'
+								value={this.state.activeItem.title}
+								onChange={this.handleChange}
+								placeholder='Enter Todo Title'
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label for='todo-description'>Description</Label>
+							<Input
+								type='text'
+								id='todo-description'
+								name='description'
+								value={this.state.activeItem.description}
+								onChange={this.handleChange}
+								placeholder='Enter Todo description'
+							/>
+						</FormGroup>
+						<FormGroup check>
+							<Label check>
+								<Input
+									type='checkbox'
+									name='completed'
+									checked={this.state.activeItem.completed}
+									onChange={this.handleChange}
+								/>
+								Completed
+							</Label>
+						</FormGroup>
+					</Form>
+				</ModalBody>
+				<ModalFooter>
+					<Button color='success' onClick={() => onSave(this.state.activeItem)}>
+						Save
+					</Button>
+				</ModalFooter>
+			</Modal>
 		);
 	}
 }
-
-export default App;
